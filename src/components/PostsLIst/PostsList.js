b1cee2860572c05f7a5fb25d3456c3a442fc954b
Aleksandr Demidenko <sashatro@mail.ru>
2022-11-18 22:14:18 +0300

    feat: Add PostsLost

--- src/components/PostsLIst/PostsList.js
+++ src/components/PostsLIst/PostsList.js
@@ -4,14 +4,14 @@ export const PostsList = () => {
     const posts = useSelector(state => state.posts);
 
     const renderedPosts = posts.map(post => (
-        <article className="post-excerpt" key={post.id}>
+        <article className="posts__excerpt" key={post.id}>
             <h3>{post.title}</h3>
-            <p className="post-content">{post.content.substring(0,100)}</p>
+            <p className="posts__content">{post.content.substring(0,100)}</p>
         </article>
     ))
 
-    return <section className="posts-list">
-        <h2>Сообщения</h2>
+    return <section className="posts">
+        <h2>Комментарии</h2>
         {renderedPosts}
     </section>
 }
